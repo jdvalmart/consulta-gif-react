c2e206827239cb360ec5f7703472dce3030183b8
jdvalmart <juanvalencia9411@outlook.com>
2025-09-23 02:41:40 -0500

    Inicio de un pequeño proyecto de consumir un api de gifs

---
+++ src/GitExpertApp.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const GitExpertApp = () => {
+  return (
+   <>
+        {/* titulo */}
+        <h1>GitExpertApp</h1>
+
+        {/* Input */}
+
+        {/* istado de gifs */}
+        {/* Gif Item */}
+   </>
+  )
+}
+
+export default GitExpertApp
